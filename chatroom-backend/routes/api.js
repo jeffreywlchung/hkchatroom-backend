2cae5009b63bcf370a4634df52526228acd14fa6
Jeffrey Chung <jeffreychung10a@gmail.com>
2018-02-25 14:55:45 +0800

    commit 04

--- chatroom-backend/routes/api.js
+++ chatroom-backend/routes/api.js
@@ -172,7 +172,7 @@ router.post('/appendChatroomToList', bodyParser.json(), function(req, res) {
   var userFilter = {'username': username};
 
   //QUERY: to get information of current user
-  userCollection.update(userFilter, {$addToSet: {'chatroomList': {'chatroomName': chatroomName, 'chatroomKey': chatroomKey}, $upsert: true}}, function(err, result) {
+  userCollection.update(userFilter, {$addToSet: {'chatroomList': {'chatroomName': chatroomName, 'chatroomKey': chatroomKey}}}, function(err, result) {
     if (err === null) {
       //SUCCESSFUL: Send success message in json
       res.json({'result': 'success'});
